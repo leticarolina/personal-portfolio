00db2e09a0f89f080bf8e11b2a735ce6ce5331d7
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2023-10-21 22:08:39 -0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const { gsap } = require("gsap/dist/gsap");
+
 const menuIcon = document.querySelector("#mobile-menu-icon");
 const menuLinks = document.querySelector(".menu-links");
-
+console.log(gsap);
 menuIcon.addEventListener("click", () => {
   toogle();
 });
@@ -12,3 +14,7 @@ function toogle() {
     menuLinks.classList.remove("open");
   });
 }
+
+// gsap.to(".box", {
+//   x: 200,
+// });
