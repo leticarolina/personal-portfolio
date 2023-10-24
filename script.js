e057a80a89268df8d76290362096725f83f8a79a
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2023-10-24 14:09:16 -0300

    animation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menuIcon = document.querySelector("#mobile-menu-icon");
 const menuLinks = document.querySelector(".menu-links");
 const svg = document.querySelector("svg");
-
+// const iMakeWebsites = document.querySelector(".description");
 menuIcon.addEventListener("click", () => {
   toogle();
 });
@@ -14,5 +14,23 @@ function toogle() {
   });
 }
 
-gsap.to(svg, { duration: 1, scale: -1, ease: "bounce" });
-gsap.from(".section_texts", { duration: 1, scale: 0 });
+//screen size checkder
+// function checkWindowSize() {
+//   if (window.innerWidth < 500) {
+//     console.log("small");
+//   }
+// }
+// checkWindowSize();
+
+gsap.from(svg, { duration: 1, scale: -1, ease: "bounce" });
+gsap.from(".hello, .name", { duration: 0.5, scale: 0, delay: 0.5 });
+
+let cursor = gsap.to(".cursor", {
+  duration: 0.6,
+  opacity: 0,
+  ease: "power2.inOut",
+  repeat: -1,
+});
+// var tl = new TimelineMax({
+//   paused:true
+// });
