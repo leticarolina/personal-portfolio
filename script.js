4fc8f2080a648fe5b80e36d6a2e9b4a4fcbf4336
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2023-10-24 22:45:30 -0300

    hero

--- script.js
+++ script.js
@@ -22,15 +22,45 @@ function toogle() {
 // }
 // checkWindowSize();
 
-gsap.from(svg, { duration: 1, scale: -1, ease: "bounce" });
-gsap.from(".hello, .name", { duration: 0.5, scale: 0, delay: 0.5 });
+// const timeline = gsap.timeline({ defaults: { duration: 1 } });
+// timeline
+//   .from(svg, { duration: 1.5, scale: 0, ease: "bounce" })
+//   .from(".hello, .name, .description", {
+//     duration: 0.5,
+//     scale: 0,
+//   })
+//   .fromTo(
+//     ".icon-arrow",
+//     { y: "10", opacity: 0 },
+//     { y: 0, repeat: -1, opacity: 100, duration: 0.9 },
+//     "< 1 "
+//   );
+// .from(".button", { duration: 1, scale: 0 });
 
-let cursor = gsap.to(".cursor", {
-  duration: 0.6,
-  opacity: 0,
-  ease: "power2.inOut",
-  repeat: -1,
-});
+// let cursor = gsap.to(".cursor", {
+//   duration: 0.6,
+//   opacity: 0,
+//   ease: "power2.inOut",
+//   repeat: -1,
+// });
+
+// var tl = new TimelineMax({}); //a loose paused tween});
+
+// tl.fromTo(
+//   ".description",
+//   8,
+//   {
+//     width: "0",
+//   },
+//   {
+//     width: "200" /* same as CSS .line-1 width */,
+//     ease: SteppedEase.config(30),
+//   },
+//   0
+// );
+
+// tl.play();
+
 // var tl = new TimelineMax({
 //   paused:true
 // });
