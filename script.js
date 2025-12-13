feccc89ff655432f95df84fc4ecf1ec4e1183626
Leticia Azevedo <leticiac@email.com>
2025-12-13 12:37:39 -0300

    update: redesigned hero section, fix responsive image behavior, and included new Smart Marriage project

--- script.js
+++ script.js
@@ -11,21 +11,22 @@ function runAnimation() {
   tl
     // name + hello pop
     // .from(".hello, .name", { scale: 0, duration: 0.9, ease: "bounce" })
-    .to(".hello, .name", { autoAlpha: 1, scale: 1, duration: 0.8, ease: "bounce" })
+    .to(".name", { autoAlpha: 1, scale: 1, duration: 0.8, ease: "bounce" })
+    .to(".name", { autoAlpha: 1, scale: 1, duration: 0.8, ease: "bounce" })
 
     // blinking cursor
-    .fromTo(
-      "#cursor",
-      { autoAlpha: 0, x: -12 },
-      { autoAlpha: 1, duration: 0.8, repeat: -1, ease: SteppedEase.config(1) },
-      "<" // start at the same time as the name pop, keep blinking forever
-    )
+    // .fromTo(
+    //   "#cursor",
+    //   { autoAlpha: 0, x: -20 },
+    //   { autoAlpha: 1, duration: 0.8, repeat: -1, ease: SteppedEase.config(1) },
+    //   "<" // start at the same time as the name pop, keep blinking forever
+    // )
 
     // type “Web3 Developer”
-    .to(".description", {
-      text: { value: "Web3 Developer" },
-      duration: 2.4,
-    })
+    // .to(".name", {
+    //   text: { value: "Azevedo" },
+    //   duration: 2.0,
+    // })
 
     // label right after typing
     .add("afterType")
@@ -35,14 +36,14 @@ function runAnimation() {
       ".hero-flower-left",
       { autoAlpha: 0, x: "-24px" },
       { autoAlpha: 1, x: "0px", duration: 0.5 },
-      "afterType+=0"
+      "<"
     )
-    .fromTo(
-      ".hero-flower-right",
-      { autoAlpha: 0, x: "24px" },
-      { autoAlpha: 1, x: "0px", duration: 0.8 },
-      "afterType+=0.1"
-    );
+  // .fromTo(
+  //   ".hero-flower-right",
+  //   { autoAlpha: 0, x: "24px" },
+  //   { autoAlpha: 1, x: "0px", duration: 0.8 },
+  //   "afterType+=0.1"
+  // );
 }
 
 // run on full load to avoid the flash
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,11 @@ module.exports = {
   content: ["./*.html", "./**/*.html"], // add more globs if needed
   theme: { extend: {} },
   plugins: [],
+  mode: "jit",  // enable JIT
+  safelist: [
+    'text-[110px]',
+    'text-[160px]',
+    // add any other dynamic classes you’re using
+  ]
 };
 
